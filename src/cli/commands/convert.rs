7f445eed404c33e7bb260ee9fb68da9d70a55dc4
Rafael Borges <rafael.f.borges@proton.me>
2026-04-02 14:43:41 -0300

    feat: loading spinner in convert command

--- src/cli/commands/convert.rs
+++ src/cli/commands/convert.rs
@@ -37,13 +37,22 @@ pub fn run(matches: &ArgMatches) -> anyhow::Result<()> {
         anyhow::bail!("Output path does not exist");
     }
 
+    let spinner = indicatif::ProgressBar::new_spinner();
+
+    spinner.set_message("Converting to image to ASCII...");
+    spinner.enable_steady_tick(std::time::Duration::from_millis(100));
+
     let gen_ascii: Vec<char> = core::ascii::gen_from_image(input)?;
+
+    spinner.set_message("Writing output...");
 
     let write_output = output
         .join(input.file_stem().unwrap())
         .with_extension("txt");
 
     fs::ascii_to_txt::write(&gen_ascii, &write_output)?;
+
+    spinner.finish_with_message("Conversion complete!");
 
     Ok(())
 }
